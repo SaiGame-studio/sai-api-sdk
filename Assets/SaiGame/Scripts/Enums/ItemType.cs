5d7d5ee5631d0fad2dd20ca19ec913ea29be8cfd
Simon Sai <tranminhduc18116@gmail.com>
2025-06-25 15:05:24 +0700

    Add Item Type

---
+++ Assets/SaiGame/Scripts/Enums/ItemType.cs
@@ -0,0 +1,11 @@
+public enum ItemType
+{
+    CharProfile, // char_profile
+    Equipment,   // equipment
+    QuestItem,   // quest_item
+    Inventory,   // inventory
+    Currency,    // currency
+    Misc,        // misc
+    LootBox,     // loot_box
+    FixLootBox   // fix_loot_box
+}
